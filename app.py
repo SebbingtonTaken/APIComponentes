c9609640c4fbaa6b709db678453b3b26ccce05e4
SebbingtonTaken <128566685+SebbingtonTaken@users.noreply.github.com>
2024-10-25 11:03:42 -0600

    Update app.py
    back to basics

--- app.py
+++ app.py
@@ -1,23 +1,14 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
+  GNU nano 7.2                                           app.py                                                    from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
-pokemon_put_args = reqparse.RequestParser()
-pokemon_put_args.add_argument("name", type=str, help="Enter name",required=True)
-pokemon_put_args.add_argument("desc", type=str, help="Enter desc",required=True)
-
-pokemons = {}
-
 class Pokemon(Resource):
-    def get(self, pokemon_id):
-        return {}
-    def put(self, pokemon_id):
-        args = pokemon_put_args.parse_args()
-        return{pokemon_id: args}
+    def get(self):
+        return {"data": "Soy un Pokemon!"}
 
-api.add_resource(Pokemon, "/helloworld/<int:pokemon_id>")
+api.add_resource(Pokemon, "/helloworld")
 
 
 
