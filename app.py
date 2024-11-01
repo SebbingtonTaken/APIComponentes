b0a5a9720657b26c3627e6faab8c3a577e22cf07
SebbingtonTaken <128566685+SebbingtonTaken@users.noreply.github.com>
2024-10-31 19:41:04 -0600

    Testing connection to dynamo DB
    Testing connection to dynamo DB with get method on Server

--- app.py
+++ app.py
@@ -1,14 +1,56 @@
-from flask import Flask
-from flask_restful import Api, Resource
+import boto3
+from flask import Flask, Response
+from flask_restful import Api, Resource, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
+import json
+
 
 app = Flask(__name__)
 api = Api(app)
+dynamodb = boto3.resource('dynamodb')
 
+# on the table resource are accessed or its load() method is called.
+table = dynamodb.Table('Users')
+
+table_creation = table.creation_date_time
+print(table.creation_date_time)
+
+pokemons = {}
+pokemon_put_args = reqparse.RequestParser()
+pokemon_put_args.add_argument("name", type=str, help="Please include the name of the pokemon", required=True)
+
+def abort_if_pokemon_doesnt_exist(pokemon_id):
+    if pokemon_id not in pokemons:
+        abort(Response("The Pokemon ID entered is invalid",404))
 class Pokemon(Resource):
-    def get(self):
-        return {"data": "Soy un Entrenador Pokemon que hace deploy desde github!"}
+    def get(self,pokemon_id):
+        #abort_if_pokemon_doesnt_exist(pokemon_id)
+        dt_str = table_creation.strftime("%Y-%m-%d %H:%M:%S")
+        json_data = json.dumps(dt_str)  
+        return json_data
+    #pokemons[pokemon_id]
+    def put(self, pokemon_id):
+        abort_if_pokemon_doesnt_exist(pokemon_id)
+        args = pokemon_put_args.parse_args()
+        pokemons[pokemon_id] = {"name": args["name"]}
+        return pokemons[pokemon_id], 201
+    def post(self,pokemon_id):
+        args = pokemon_put_args.parse_args()
+        pokemons[pokemon_id] = {"name": args["name"]}
+        return pokemons[pokemon_id], 201
+    def delete(self, pokemon_id):
+        abort_if_pokemon_doesnt_exist(pokemon_id)
+        del pokemons[pokemon_id]
+        return '', 204
+api.add_resource(Pokemon, "/helloworld/<int:pokemon_id>")
 
-api.add_resource(Pokemon, "/helloworld")
+# Get the service resource.
+
+
+# Instantiate a table resource object without actually
+# creating a DynamoDB table. Note that the attributes of this table
+# are lazy-loaded: a request is not made nor are the attribute
+# values populated until the attributes
 
 
 
--- test.py
+++ test.py
@@ -4,6 +4,6 @@ import requests
 BASE = "http://127.0.0.1:5000/"
 
 
-response = requests.put(BASE + "helloworld/1",{"desc": "Numbah 1"})
+response = requests.put(BASE + "helloworld/1",{"name": "Bulbasaur"})
 print(response.json())
 print(BASE)
